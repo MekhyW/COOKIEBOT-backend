1f43ec35f109feebf2ee66aeb2dff8e27f5aadf4
myghi63 <110261785+myghi63@users.noreply.github.com>
2022-10-10 11:33:58 -0300

    First Commit
    Add files auto-generated by STS

---
+++ src/main/java/com/cookiebot/COOKIEBOTbackend/CookiebotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cookiebot.COOKIEBOTbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiebotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookiebotBackendApplication.class, args);
+	}
+
+}

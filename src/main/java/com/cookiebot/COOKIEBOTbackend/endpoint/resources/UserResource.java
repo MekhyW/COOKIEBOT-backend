0cb5f585e6f90a9a83a5e7ad93b341684cadc708
myghi63 <110261785+myghi63@users.noreply.github.com>
2022-10-11 13:26:09 -0300

    Implemented DTO

--- src/main/java/com/cookiebot/COOKIEBOTbackend/endpoint/resources/UserResource.java
+++ src/main/java/com/cookiebot/COOKIEBOTbackend/endpoint/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.cookiebot.COOKIEBOTbackend.endpoint.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cookiebot.COOKIEBOTbackend.dao.services.UserService;
 import com.cookiebot.COOKIEBOTbackend.endpoint.domain.User;
+import com.cookiebot.COOKIEBOTbackend.endpoint.dto.UserDTO;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,8 +22,9 @@ public class UserResource {
 
 	// Accessible with localhost:8080/users
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }

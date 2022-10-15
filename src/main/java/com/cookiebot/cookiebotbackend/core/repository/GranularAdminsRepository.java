d75d0dbb76eca9efde254b505b98bcfe458b1341
myghi63 <110261785+myghi63@users.noreply.github.com>
2022-10-15 19:23:40 -0300

    GranularAdmins feature drop

--- src/main/java/com/cookiebot/cookiebotbackend/core/repository/GranularAdminsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cookiebot.cookiebotbackend.core.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cookiebot.cookiebotbackend.core.domain.GranularAdmins;
-
-public interface GranularAdminsRepository extends MongoRepository<GranularAdmins, String> {
-
-}

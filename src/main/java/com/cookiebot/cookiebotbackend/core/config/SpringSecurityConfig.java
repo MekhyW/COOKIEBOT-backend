5c57c39ae3e899ceab2a91cd0f079e12d8c37551
myghi63 <110261785+myghi63@users.noreply.github.com>
2022-11-17 15:22:08 -0300

    Implement Simple Authentication
    In order to work, it needs these parameters on application.properties:
    spring.security.user.name=username
    spring.security.user.password=password
    spring.security.user.roles=ADMIN

---
+++ src/main/java/com/cookiebot/cookiebotbackend/core/config/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cookiebot.cookiebotbackend.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+				.authorizeRequests(auth -> {
+					auth.antMatchers("/**").hasRole("ADMIN");
+				})
+				.httpBasic(Customizer.withDefaults())
+				.build();
+	}
+}

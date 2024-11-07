7b43cdd26fcf5b6efda8c3225a81be797141b576
Dangerousplay <dangerousplay715@gmail.com>
2024-11-07 18:16:23 -0300

    doc(swagger): added git tag version

--- src/main/java/com/cookiebot/cookiebotbackend/core/config/SwaggerConfiguration.java
+++ src/main/java/com/cookiebot/cookiebotbackend/core/config/SwaggerConfiguration.java
@@ -4,18 +4,24 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration
 public class SwaggerConfiguration {
+    private static final String GIT_BUILD_VERSION = "build.version";
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI springShopOpenAPI(final Optional<GitProperties> gitProperties) {
+        final var version = gitProperties.map(p -> p.get(GIT_BUILD_VERSION)).orElse("develop");
+
         return new OpenAPI()
                 .info(new Info().title("Cookiebot backend API")
                         .description("Cookiebot backend services")
-                        .version("v1.2.1")
+                        .version(version)
                         .license(new License().name("Apache 2.0")))
                 .externalDocs(new ExternalDocumentation()
                         .description("Cookiebot backend README")

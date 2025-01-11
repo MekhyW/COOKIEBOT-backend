b0bd29b20798c3bf7e31b1d302b68f5c3c01407e
Davi Henrique <dangerousplay715@gmail.com>
2025-01-10 22:30:46 -0300

    doc(swagger): added documentation for bff config endpoints

--- src/main/java/com/cookiebot/cookiebotbackend/bff/controller/ConfigsController.java
+++ src/main/java/com/cookiebot/cookiebotbackend/bff/controller/ConfigsController.java
@@ -2,11 +2,18 @@ package com.cookiebot.cookiebotbackend.bff.controller;
 
 import com.cookiebot.cookiebotbackend.core.domains.Config;
 import com.cookiebot.cookiebotbackend.dao.services.ConfigService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
+@Tag(name = "BFF bot config", description = "BFF telegram bot configuration API")
 @RestController
 @RequestMapping("bff/group")
 public class ConfigsController {
@@ -16,12 +23,28 @@ public class ConfigsController {
         this.configService = configService;
     }
 
+    @Operation(summary = "Get configuration", description = "Get group bot configuration")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Config.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Group does not exist"),
+            @ApiResponse(responseCode = "403", description = "User does not have permission to access bot configuration")
+    })
     @GetMapping("/{groupId}/config")
     @PreAuthorize("@authz.isAdminOfGroup(#root, #groupId)")
     public Config getGroupConfig(@PathVariable String groupId) {
         return this.configService.findById(groupId);
     }
 
+    @Operation(summary = "Update configuration", description = "Update bot configuration for group")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Config.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Group does not exist"),
+            @ApiResponse(responseCode = "403", description = "User does not have permission to update bot configuration")
+    })
     @PutMapping("/{groupId}/config")
     @PreAuthorize("@authz.isAdminOfGroup(#root, #groupId)")
     public Config updateGroupConfig(@PathVariable String groupId, @RequestBody @Valid Config config) {

2096d66b4738224322b1ad6a56abd8e5a734ed99
Davi Henrique <dangerousplay715@gmail.com>
2025-01-10 22:30:59 -0300

    doc(swagger): added documentation for bff group endpoints

--- src/main/java/com/cookiebot/cookiebotbackend/bff/controller/GroupsController.java
+++ src/main/java/com/cookiebot/cookiebotbackend/bff/controller/GroupsController.java
@@ -2,6 +2,13 @@ package com.cookiebot.cookiebotbackend.bff.controller;
 
 import com.cookiebot.cookiebotbackend.core.domains.Group;
 import com.cookiebot.cookiebotbackend.dao.services.GroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Stream;
 
+@Tag(name = "BFF groups", description = "BFF telegram groups API")
 @RestController
 @RequestMapping("bff")
 public class GroupsController {
@@ -20,6 +28,12 @@ public class GroupsController {
         this.groupService = groupService;
     }
 
+    @Operation(summary = "Get user groups", description = "Get groups where user can edit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Group.class)))
+            })
+    })
     @GetMapping("/my-groups")
     public Stream<Group> getMyGroups(@AuthenticationPrincipal Jwt jwt) {
         return this.groupService.findGroupsUserIsAdmin(jwt.getSubject());

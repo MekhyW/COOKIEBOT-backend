565791c46e4ab041aef8d051bc098702c71b11b5
Myghi63 <myghi63@disroot.org>
2025-02-12 17:44:06 -0300

    Refactor: Updated pom.xml, replaced deprecated annotations, remove public from beans

--- src/test/java/com/cookiebot/cookiebotbackend/bff/controller/GroupsControllerTest.java
+++ src/test/java/com/cookiebot/cookiebotbackend/bff/controller/GroupsControllerTest.java
@@ -1,31 +1,32 @@
 package com.cookiebot.cookiebotbackend.bff.controller;
 
-import com.cookiebot.cookiebotbackend.bff.service.AuthorizationService;
-import com.cookiebot.cookiebotbackend.core.domains.Group;
-import com.cookiebot.cookiebotbackend.dao.services.GroupService;
-import com.cookiebot.cookiebotbackend.test.BffControllerTest;
-import com.cookiebot.cookiebotbackend.utils.BeanUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.util.List;
-import java.util.stream.Stream;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.cookiebot.cookiebotbackend.bff.service.AuthorizationService;
+import com.cookiebot.cookiebotbackend.core.domains.Group;
+import com.cookiebot.cookiebotbackend.dao.services.GroupService;
+import com.cookiebot.cookiebotbackend.test.BffControllerTest;
+import com.cookiebot.cookiebotbackend.utils.BeanUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @BffControllerTest
 @WebMvcTest(value = GroupsController.class)
@@ -39,7 +40,7 @@ class GroupsControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private GroupService groupService;
 
     @BeforeEach

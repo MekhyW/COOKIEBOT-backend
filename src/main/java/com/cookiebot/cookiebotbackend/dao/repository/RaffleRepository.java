20490f77438bb84522ab76b8608ecaeba972401c
Myghi63 <myghi63@disroot.org>
2025-02-24 10:16:53 -0300

    Remove Raffles (deprecated)

--- src/main/java/com/cookiebot/cookiebotbackend/dao/repository/RaffleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cookiebot.cookiebotbackend.dao.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cookiebot.cookiebotbackend.core.domains.Raffle;
-
-public interface RaffleRepository extends MongoRepository<Raffle, String> {
-
-}
